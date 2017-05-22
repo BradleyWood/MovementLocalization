867874b31a1f460eac44c05b6b89ef4dbdd4ba57
Brad <bradley.wood@uoit.net>
2017-05-22 06:13:32 -0400

    Initial Commit

---
+++ src/main/java/localization/App.java
@@ -0,0 +1,13 @@
+package localization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

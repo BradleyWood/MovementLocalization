eb05b9308c455fde62664a7c13ab491765a0f0c2
Brad <bradley.wood@uoit.net>
2017-07-12 04:21:43 -0400

    Ball test

--- src/main/java/localization/App.java
+++ src/main/java/localization/App.java
@@ -1,10 +1,53 @@
 package localization;
 
 
-public class App 
+import localization.processor.DefaultMovementLocalizer;
+import localization.source.BallTest;
+import localization.source.InputSource;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+/**
+ * A simple test to detect moving balls
+ */
+public class App
 {
-    public static void main(String[] args )
-    {
-        
+
+    private static InputSource source = new BallTest(800,700, BufferedImage.TYPE_INT_ARGB);
+    private static DefaultMovementLocalizer dml;
+    private static Display d;
+
+    public static void main(String[] args) {
+        d = new Display();
+        dml = new DefaultMovementLocalizer(source.getImage());
+
+        new Timer(50, timedEvent).start();
+
+        JFrame frame = new JFrame("Movement Localization");
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(d);
+        frame.setVisible(true);
     }
+    static ActionListener timedEvent = e-> {
+        BufferedImage img = source.getImage();
+        dml.update(img);
+        BufferedImage gg = copy(img);
+        Graphics g = gg.getGraphics();
+        g.setColor(Color.red);
+        for(Rectangle r : dml.localize()) {
+            g.drawRect(r.x, r.y, r.width, r.height);
+        }
+        d.update(gg);
+        d.repaint();
+    };
+    private static BufferedImage copy(BufferedImage source) {
+        BufferedImage image = new BufferedImage(source.getWidth(), source.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        image.getGraphics().drawImage(source,0,0, d);
+        return image;
+    }
+
 }
